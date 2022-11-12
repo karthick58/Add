0af8b51607a0a6c3c6bda3b9ab3b5d23b8365c38
kali <kali@kali.kali>
2022-11-12 13:30:32 +0530

    new

---
+++ inheritance/Multiple.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+  
+interface one {
+    public void print_geek();
+}
+  
+interface two {
+    public void print_for();
+}
+  
+interface three extends one, two {
+    public void print_geek();
+}
+class child implements three {
+    @Override public void print_geek()
+    {
+        System.out.println("Geeks");
+    }
+  
+    public void print_for() { System.out.println("for"); }
+}
+  
+// Drived class
+public class Multiple {
+    public static void main(String[] args)
+    {
+        child c = new child();
+        c.print_geek();
+        c.print_for();
+        c.print_geek();
+    }
+}
